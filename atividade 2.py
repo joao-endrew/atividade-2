582ad693d80156184535a4da038a1df65c4b8f27
joao-endrew <endrewjoao@gmail.com>
2025-05-03 20:00:28 -0300

    Update atividade 2.py

--- atividade 2.py
+++ atividade 2.py
@@ -1,9 +1,7 @@
-
-
 intro= ("aqui está os números selecionados: ")
 print(intro)
 
-for par in range(0, 31, 2):
+for par in range(0, 30, 2):
     if par in [10, 20, 30]:
         continue
     print(par)
